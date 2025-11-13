854468600102c1304613f1303fa5f6886be9e8bd
Isabella Maria <imbrodrigues.mbr@gmail.com>
2025-11-12 21:56:09 -0300

    adicionado botão

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,19 @@
+export const HomePage = () => {
+    return (
+        <main className="min-h-dvh grid place-items-center bg-slate-50">
+            <h1 className="text-3xl font-bold text-slate-800">
+               Escolher é difícil, assistir não precisa ser!
+            </h1>
+
+
+<button type="submit" className="bg-amber-800 text-white p-2 r
+ounded">COMEÇAR</button>
+
+
+
+      <footer class="rodape">(Não dê Match no Tinder... Dê Match com a gente)</footer>
+        </main>
+    );
+    
+}
+
