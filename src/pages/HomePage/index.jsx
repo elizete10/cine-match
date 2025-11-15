ecd280be3b54713a77f133b4e3f44a8ea4ce0669
Isabella Maria <imbrodrigues.mbr@gmail.com>
2025-11-14 21:20:46 -0300

    atualizado a primeira página e botões

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,19 +1,29 @@
+import { Link } from "react-router-dom";
+
 export const HomePage = () => {
     return (
-        <main className="min-h-dvh grid place-items-center bg-slate-50">
-            <h1 className="text-3xl font-bold text-slate-800">
-               Escolher é difícil, assistir não precisa ser!
-            </h1>
+        <main className="min-h-dvh bg-linear-to-r from-stone-900 to-stone-950 flex items-center justify-center p-4">
+            <div className="w-full max-w-4xl text-center space-y-8">
+                <header className="space-y-4">
+                    <h1 className="text-6xl md:text-7xl text-neutral-50 font-bold tracking-tight">Cine Match</h1>
+                    <h2 className="text-2xl md:text-3xl font-medium text-violet-800">
+                        Escolher é difícil, assistir não precisa ser!
+                    </h2>
+                </header>
 
+                <Link to="/questoes"
+                    className="inline-flex items-center px-8 py-4 bg-linear-to-r from-violet-600 to-violet-800 hover:scale-105 text-neutral-50 text-xl font-semibold rounded-lg shadow-lg shadow-violet-500 transform transition-all duration-300 hover:shadow-xl">
+                    Começar Agora
+                </Link>
 
-<button type="submit" className="bg-amber-800 text-white p-2 r
-ounded">COMEÇAR</button>
-
-
-
-      <footer class="rodape">(Não dê Match no Tinder... Dê Match com a gente)</footer>
+                <footer className="pt-12">
+                    <p className="text-sm text-neutral-100">
+                        &copy; Não dê Match no Tinder... Dê Match com a gente
+                    </p>
+                </footer>
+            </div>
         </main>
     );
-    
+
 }
 
